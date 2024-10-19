3445d7e3bb7b221cfb2fef042692b59f6ffc8af2
Gabriel Melo <bielrj170@gmail.com>
2024-10-19 12:51:52 -0300

    refactor: change server file to inside src folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
-import { app } from "./src/app.js";
+import { app } from "./app.js";
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
+	console.log("oi");
+	console.log("oi2");
+
 });
+
