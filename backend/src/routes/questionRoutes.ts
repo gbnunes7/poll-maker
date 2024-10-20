290d47ae6fb65c308ccc277ace8af13ac711cded
Gabriel Melo <bielrj170@gmail.com>
2024-10-20 19:11:52 -0300

    feat: add dedicated routes to question controller

---
+++ backend/src/routes/questionRoutes.ts
@@ -0,0 +1,32 @@
+import QuestionController from "controllers/question";
+import routes from "./questionaryRoutes";
+import { Request, Response } from "express";
+
+const questionController = new QuestionController();
+
+routes.get(
+	"/question",
+	async (req: Request, res: Response) =>
+		await questionController.getAll(req, res)
+);
+
+routes.get(
+	"/question/id/:id",
+	async (req: Request, res: Response) =>
+		await questionController.getById(req, res)
+);
+
+routes.put("/question/update/id/:id", async (req: Request, res: Response) => {
+	await questionController.update(req, res);
+});
+
+routes.delete(
+	"/question/delete/id/:id",
+	async (req: Request, res: Response) => {
+		await questionController.delete(req, res);
+	}
+);
+
+routes.post("/question/new", async (req: Request, res: Response) => {
+	await questionController.create(req, res);
+});
