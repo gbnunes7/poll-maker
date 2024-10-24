725bc7505989ef746c344b6807979259b295044b
Gabriel Melo <bielrj170@gmail.com>
2024-10-24 01:07:14 -0300

    feat: create a generic title component

---
+++ frontend/src/components/Title/index.tsx
@@ -0,0 +1,12 @@
+interface TitleProps {
+	level: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+	children: React.ReactNode;
+	className?: string;
+}
+
+const Title: React.FC<TitleProps> = ({ children, level, ...rest }) => {
+	const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+	return <Tag {...rest}>{children}</Tag>;
+};
+
+export default Title;
