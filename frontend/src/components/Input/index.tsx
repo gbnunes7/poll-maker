be620818fed83852212d21877c8c129bb58f6636
Gabriel Melo <bielrj170@gmail.com>
2024-11-03 14:48:24 -0300

    refacotr: refactor input form

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,25 +1,11 @@
-interface InputProps {
-	children?: React.ReactNode;
-	type?: string;
-	placeholder?: string;
-	id?: string;
-	name?: string;
-	value?: string;
-	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-	className?: string;
-	disabled?: boolean;
-	required?: boolean;
-	maxLength?: number;
-	onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
-	onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
-	autoComplete?: string;
-	pattern?: string;
-    minLength?: number;
-	ref?: React.Ref<HTMLInputElement>;
-}
+import React from 'react';
 
-const Input: React.FC<InputProps> = ({ ...rest }) => {
-	return <input ref={rest.ref} {...rest} />;
-};
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ ...props }, ref) => <input ref={ref} {...props} />
+);
+
+Input.displayName = 'Input';
 
 export default Input;
